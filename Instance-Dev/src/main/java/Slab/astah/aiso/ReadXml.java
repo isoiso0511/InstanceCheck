033c12bc4cb4c3f029d963827931610e546aa19a
iso <aiso@sakailab.info>
2018-11-20 16:12:12 +0900

    saxも追加

--- Instance-Dev/src/main/java/Slab/astah/aiso/ReadXml.java
+++ Instance-Dev/src/main/java/Slab/astah/aiso/ReadXml.java
@@ -4,22 +4,28 @@ import java.io.File;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+//saxの使用
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
+//domの使用
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-//DOMの使用
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class ReadXml{
 	public static void main(String[] args) throws Exception{
 		ReadXml xml = new ReadXml();
 	}
 	ReadXml(){
-		read();
+		readDom();
+		readSax();
 	}
 
-	private void read() {
+	private void readDom() {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		try {
 			DocumentBuilder builder = factory.newDocumentBuilder();
@@ -45,4 +51,39 @@ public class ReadXml{
 		}
 	}
 
+	private void readSax() {
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		try {
+			SAXParser parser = factory.newSAXParser();
+			parser.parse(new File("test.xml"), new SaxXml());
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+}
+
+class SaxXml extends DefaultHandler{
+	public void startDocument() {
+		System.out.println("読み込み開始");
+	}
+
+	public void endDocument() {
+		System.out.println("読み込み終了");
+	}
+
+	public void startElement(String uri, String localName, String qName, Attributes attributes) {
+		System.out.println("Node: " + qName + " 開始");
+		if(qName.equals("info")) {
+			System.out.println(attributes.getQName(0) + ": " + attributes.getValue(0));
+		}
+	}
+
+	public void endElement(String uri, String localName, String qName) {
+		System.out.println("Node: " + qName + " 終了");
+	}
+
+	public void characters(char[] ch, int start, int length) {
+		System.out.println(new String(ch, start, length));
+	}
 }
